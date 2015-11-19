23c191dcfd546eb7d5bae0dd1523984be8d25383
Nicolai Østergaard <nicolai76@live.dk>
2015-11-19 21:05:30 +0100

    Changed the layout in the UserLogin class to setLayout(new FlowLayout()); and deleted all the setBounds since they doesnt have any effect.

--- Snake/GUI/UserLogin.java
+++ Snake/GUI/UserLogin.java
@@ -23,60 +23,39 @@ public class UserLogin extends JPanel
     {
         Condition co = new Condition();
 
-     /*
-        int screenWidth = co.getScreenWidth();
-        int screenHeight = co.getScreenHeight();
-
-        double width = 0.109375*screenWidth;
-        double height = 0.033333*screenHeight;
-
-        int txt_width = (int) width;
-        int txt_height = (int) height;
-
-        System.out.println(txt_width);
-        System.out.println(txt_height);
-        */
         setLayout(new FlowLayout());
         setBackground(Color.GREEN);
 
         jlUserLogin = new JLabel("LOGIN");
-        jlUserLogin.setBounds(419, 155, 78, 33);
         add(jlUserLogin);
 
         jlUsername = new JLabel("Username/E-mail");
-        jlUserLogin.setBounds(398, 290, 120, 33);
         add(jlUserLogin);
 
         Username = new JTextField("Enter username here");
-        Username.setBounds(340, 333, 100, 100);
         Username.setFont(new Font("Italic", Font.ITALIC, 15));
         add(Username);
         Username.setColumns(25);
 
         jlPassword = new JLabel("Password");
-        jlPassword.setBounds(398, 453, 115, 33);
         add(jlPassword);
 
         Password = new JTextField("Enter password here");
-        Password.setBounds(340, 496, 100, 100);
         Password.setFont(new Font("Bold", Font.BOLD, 15));
         add(Password);
         Password.setColumns(11);
 
         btnSignIn = new JButton("Sign in ->");
-        btnSignIn.setBounds(372, 621, 171, 41);
         add(btnSignIn);
 
         btnSignUp = new JButton("Sign up ->");
         add(btnSignUp);
 
         jlSnake = new JLabel("SNAKE");
-        jlSnake.setBounds(300, 300, 300, 300);
         add(jlSnake);
 
         Icon bg = new ImageIcon(getClass().getResource("bg.png"));
         jlBg = new JLabel("", bg, JLabel.CENTER);
-        jlBg.setBounds(100, 100, 100, 100);
         add(jlBg);
 
 
