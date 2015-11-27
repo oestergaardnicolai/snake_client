1c3b66a8fb403a10988ca9408e9cedf846789b33
Nicolai Østergaard <nicolai76@live.dk>
2015-11-27 12:00:17 +0100

    SDK controller startup

---
+++ Snake/LOGIC/sdk_controller.java
@@ -0,0 +1,17 @@
+package snake_client.Snake.LOGIC;
+
+import com.google.gson.Gson;
+import snake_client.Snake.SDK.ServerConnection;
+
+/**
+ * Created by nicolaiostergaard on 27/11/15.
+ */
+public class sdk_controller {
+
+    public static void login(String username, String password)
+    {
+        ServerConnection sc = new ServerConnection();
+
+        User 
+    }
+}
